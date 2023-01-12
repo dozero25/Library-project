41ce5a0ab4f6378c2f143772d2fe86164757ff5a
dozero25 <ehdud05264@naver.com>
2023-01-12 21:53:53 +0900

    이름 추

--- libraryAca/library/src/main/java/com/korit/library/web/dto/SearchReqDto.java
+++ libraryAca/library/src/main/java/com/korit/library/web/dto/SearchReqDto.java
@@ -7,23 +7,23 @@ import javax.validation.constraints.NotBlank;
 
 @Data
 public class SearchReqDto {
-    @ApiModelProperty(required = false, example = "나는")
+    @ApiModelProperty(value = "검색어", required = false, example = "나는")
     private String searchValue;
 
-    @ApiModelProperty(required = false, example = "소설")
+    @ApiModelProperty(value = "카테고리", required = false, example = "소설")
     private String category;
 
-    @ApiModelProperty(required = false, example = "bookName")
+    @ApiModelProperty(value = "정렬", required = false, example = "bookName")
     private String order;
 
     @NotBlank
-    @ApiModelProperty(required = true, notes = "조회 전체 = N, 조회제한 = Y")
+    @ApiModelProperty(value = "조회 전체 = N, 조회제한 = Y", required = true)
     private String limit;
 
-    @ApiModelProperty(required = false, example = "1")
+    @ApiModelProperty(value = "페이지 번호",required = false, example = "1")
     private int page;
 
-    @ApiModelProperty(required = false, example = "20")
+    @ApiModelProperty(value = "게시글 개수", required = false, example = "20")
     private int count;
 
     @ApiModelProperty(hidden = true)
